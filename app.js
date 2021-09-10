21de5c85e2c83f6772bd029c23ca4ba0eb5d3b79
Sankeerth <sankeerth.coder@gmail.com>
2021-09-10 20:36:03 +0530

    done

---
+++ app.js
@@ -0,0 +1,11 @@
+var btnTranslate = document.querySelector("#btn-Translate")
+
+btnTranslate.addEventListener("click", function clickEventHandler(){
+    console.log("Clicked");
+})
+
+var bangme = document.querySelector("#bang-Me")
+
+bangme.addEventListener("click", function clcikBangMe(){
+    console.log("banged!");
+} )
